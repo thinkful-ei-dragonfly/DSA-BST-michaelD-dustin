e6d60a130bc2f36582ec9292a335ed7ed54b12b8
Michael Dusaniwskyj <dusaniwskyj@gmail.com>
2019-07-30 13:09:32 -0400

    created new BST with given array 3...7

--- bst-drills.js
+++ bst-drills.js
@@ -1,1 +1,13 @@
 const BST = require('./bst');
+
+const newBST = new BST;
+newBST.insert(3);
+newBST.insert(1);
+newBST.insert(4);
+newBST.insert(6);
+newBST.insert(9);
+newBST.insert(2);
+newBST.insert(5);
+newBST.insert(7);
+
+console.log(newBST);
